560c70a9d975732ad0d0e20a630f6eaf76f2bbf4
takurou1559 <taku20031559@gmail.com>
2020-02-16 23:26:26 +0900

    完成！！超うれしいです

--- app/src/main/java/com/websarva/wings/android/edittext/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/edittext/MainActivity.kt
@@ -2,14 +2,39 @@ package com.websarva.wings.android.edittext
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
+    var tList = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        add.setOnClickListener {
+            val input = findViewById<EditText>(R.id.ET)
+            val inputStr = input.text.toString()
+            tList.add(inputStr)            // ここにボタンを押したとの処理を書く
+        }
+        show.setOnClickListener {
+            val input = findViewById<EditText>(R.id.ET)
+            val inputStr = input.text.toString()
+            TV.setText(inputStr)
+            ET.setText("")
+            // ここにボタンを押したとの処理を書く
+        }
+        clear.setOnClickListener {
+           tList.clear()
+            TV.setText("")
+
+
+            // ここにボタンを押したとの処理を書く
+        }
+        }
 
     }
 
-}
+
